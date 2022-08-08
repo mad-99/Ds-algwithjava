e9a2e894e3a3d465eb1966c9abf08b811718be10
mad-99 <madhurmaheshwari1999@gmail.com>
2022-08-08 21:33:39 +0530

    8aug

--- StaticGenericMethodDemoT.java
+++ StaticGenericMethodDemoT.java
@@ -2,7 +2,8 @@ class StaticGenericMethodDemo{
 // Defining a static generic method to print any data type
 static <T> void genericPrint (T t){
 //The following statement print which type parameter T this method is handling
-System.out.println (t.getClass().getName() + ":" + t);
+System.out.println (t.getClass().getName() + ":" + t);//print statement
+
 }
 public static void main(String[] args){
 genericPrint(101); // Calling generic method with integer argument
