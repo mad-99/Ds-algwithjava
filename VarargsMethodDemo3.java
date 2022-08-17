161deb54e52164418feaaeb8472330f4440be9db
mad-99 <madhurmaheshwari1999@gmail.com>
2022-08-17 08:43:24 +0530

    mji

--- VarargsMethodDemo3.java
+++ VarargsMethodDemo3.java
@@ -5,7 +5,7 @@ System.out.print(“ “ + o);
 System.out.println( );
 }
 public static void main(String[] args) {
-varargsMethod3( 1, “String”, 2.3, true); // // Four arguments
+varargsMethod3( 1, “String”, 2.3, true); //  Four arguments
 varargsMethod3 (); // No arguments
 varargsMethod3 (15, 25, 35, 45, 55); // Five arguments
 }
